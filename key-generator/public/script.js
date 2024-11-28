dd0866044426361a344dcbe84cb77c75bb071209
MurKilDev <162470338+MuradKil@users.noreply.github.com>
2024-11-28 19:34:32 +0300

    Delete key-generator directory

--- key-generator/public/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("generateKeyButton").addEventListener("click", async () => {
-  const response = await fetch("/api/generateKey", { method: "POST" });
-  const data = await response.json();
-  document.getElementById("keyOutput").textContent = `Generated Key: ${data.key}`;
-});
